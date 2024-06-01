b22d6c8e5a6c95ca3dad165dd60e1676c58a74d9
zaknolte <127704632+zaknolte@users.noreply.github.com>
2024-06-01 11:53:54 -0700

    convert to dash app & add side menu

---
+++ app.py
@@ -0,0 +1,44 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import html
+
+app = dash.Dash(
+    __name__,
+    suppress_callback_exceptions=True,
+    external_stylesheets=[dbc.themes.DARKLY, dbc.icons.FONT_AWESOME],
+)
+
+
+sidebar = html.Div(
+    [
+        html.Div(
+            [html.I(className="fa-solid fa-sliders me-2"), html.Span("Settings")],
+            className="sidebar-header",
+        ),
+        html.Hr(),
+        dbc.Nav(
+            [
+                html.H2("test 1"),
+                html.H5("test 2")
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    className="sidebar",
+)
+
+app.layout = html.Div(
+    [
+        sidebar,
+        html.Div(
+            [
+                dash.page_container
+            ],
+            className="content",
+        ),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
