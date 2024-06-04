1a3f9cee2dacfdc69b43a9b52f9cfce9e9f9b275
zaknolte <127704632+zaknolte@users.noreply.github.com>
2024-06-03 21:16:44 -0700

    add baseline trendline

---
+++ components/baseline.py
@@ -0,0 +1,87 @@
+from dash import Dash, dcc, html, Input, Output, ALL, Patch, callback, MATCH
+import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+
+
+baseline_accordian = dmc.Accordion(
+    children=[
+        dmc.AccordionItem(
+            [
+                dmc.AccordionControl(
+                    "Baseline",
+                    icon=DashIconify(
+                        icon="mdi:graph-bell-curve-cumulative",
+                        color="black",
+                        width=20,
+                    ),
+                ),
+                dmc.AccordionPanel(
+                    [
+                        dbc.Button("+ Add Trendline", id="add-trendline", style={"width": "100%"}),
+                        html.Hr()
+                    ],
+                    id="add-baseline-accordian"
+                ),
+            ],
+            value="baseline-accordian",
+        ),
+    ]
+)
+
+
+def trendline_options(n_clicks):
+    return dmc.Accordion(
+        children=[
+            dmc.AccordionItem(
+                [
+                    dmc.AccordionControl(f"Baseline Trend {n_clicks}", id={"type": "baseline-trendline", "index": n_clicks}),
+                    dmc.AccordionPanel(
+                        [
+                            html.Div(
+                                [
+                                    html.P("Trendline X Start:", style={"margin-top": 10}),
+                                    dbc.Input(type="number", value=0, min=0, style={"width": 100, "margin-left": 20}, className="sidebar-input", id={"type": "baseline-start", "index": n_clicks})
+                                ],
+                                className="accordian-options"
+                            ),
+                            html.Div(
+                                [
+                                    html.P("Trendline X Stop:", style={"margin-top": 10}),
+                                    dbc.Input(type="number", value=0, min=0, style={"width": 100, "margin-left": 20}, className="sidebar-input", id={"type": "baseline-stop", "index": n_clicks})
+                                ],
+                                className="accordian-options"
+                            ),
+                            html.Div(
+                                [
+                                    html.P("Slope Factor:", style={"margin-top": 10}),
+                                    dbc.Input(type="number", value=0, style={"width": 100, "margin-left": 20}, className="sidebar-input", id={"type": "baseline-slope", "index": n_clicks})
+                                ],
+                                className="accordian-options"
+                            ),
+                            html.Div(
+                                [
+                                    dbc.Switch(label="Reset Baseline After?", id={"type": "reset_baseline", "index": n_clicks})
+                                ],
+                                className="accordian-options",
+                                style={"margin-top": "1rem"}
+                            ),
+                        ],
+                    ),
+                ],
+                value="baseline-accordian",
+            ),
+        ]
+    )
+
+
+@callback(
+    Output("add-baseline-accordian", "children"),
+    Input("add-trendline", "n_clicks"),
+    prevent_initial_call=True
+)
+def display_dropdowns(n_clicks):
+    patched_children = Patch()
+    trendline = trendline_options(n_clicks)
+    patched_children.append(trendline)
+    return patched_children
