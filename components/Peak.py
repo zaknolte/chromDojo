de3b08becf2a0cbc9433ae468420b3cdb367d157
zaknolte <127704632+zaknolte@users.noreply.github.com>
2024-06-22 09:13:27 -0700

    minor revision

--- components/Peak.py
+++ components/Peak.py
@@ -11,19 +11,20 @@ class Peak:
         self.center = center
         self.width = width
         self.skew = skew
-        self.y = self.create_peak()
         self.area = 0
         self.start_idx = 0
         self.stop_idx = 0
         self.concentration = 0
         self.unit = "ppm"
-        self.calibration = Calibration()
+        self.calibration = Calibration(self)
+
+        self.y = self.create_peak()
 
     def create_peak(self):
         # current formula with skewness does not draw with width = 0
         # set to minimal sharp value to improve UX and not have peaks disappearing
         if self.width == 0:
-            self.width = 0.1
+            self.width = 0.01
         # https://www.desmos.com/calculator/gokr63ciym
         return self.height * np.exp(-0.5 * ((self.x - self.center) / (self.width + (self.skew * (self.x - self.center))))**2)
         # https://www.desmos.com/calculator/k5y9glwjee   ??
