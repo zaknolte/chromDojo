80ff6b7ef9de6e5bbc2f3f37fa56532d422042d8
zaknolte <127704632+zaknolte@users.noreply.github.com>
2024-06-23 16:59:32 -0700

    add conc and units

--- components/results_panel.py
+++ components/results_panel.py
@@ -9,7 +9,6 @@ import peakutils
 from scipy.signal import find_peaks
 import jsonpickle
 
-
 results_panel = dmc.TabsPanel(
     dag.AgGrid(
         id="results-table",
@@ -37,6 +36,13 @@ results_panel = dmc.TabsPanel(
             {
                 "field": "Units",
                 "width": 140,
+                "icons": {
+                    "sortAscending": '<i class="fa fa-solid fa-pencil">',
+                    "sortDescending": '<i class="fa fa-solid fa-pencil"/>',
+                },
+                "sort": "desc",
+                "editable": True
+                # "sortable": False
             },
         ],
         dashGridOptions={
@@ -58,19 +64,35 @@ def update_results_table(graph_data):
     if graph_data is not None and any(graph_data["y"]):
         peaks = jsonpickle.decode(graph_data["peaks"])
         row_data = []
-
         for peak in peaks:
+            if peak.area == 0:
+                conc = 0
+            else:
+                conc = peak.calibration.calculate_concentration(peak.area)
             row_data.append(
                 {
                     "RT": f"{peak.center:.2f} min",
                     "Name": peak.name,
                     "Height": f"{peak.height:.2f}",
                     "Area": f"{peak.area:.2f}",
-                    "Concentration": "N/A",
-                    "Units": "N/A"
+                    "Concentration": f"{conc:.2f}",
+                    "Units": peak.calibration.units
                 }
             )
 
         return row_data
     
     return no_update
+
+@callback(
+    Output("x-y-data", "data", allow_duplicate=True),
+    Input("results-table", "cellValueChanged"),
+    State("x-y-data", "data"),
+    prevent_initial_call=True
+)
+def update_units(units, graph_data):
+    peaks = jsonpickle.decode(graph_data["peaks"])
+    for peak in peaks:
+        if peak.name == units[0]["data"]["Name"]:
+            peak.calibration.units = units[0]["data"]["Units"]
+    return {"x": graph_data["x"], "y": graph_data["y"], "peaks": jsonpickle.encode(peaks)}
