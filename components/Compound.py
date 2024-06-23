8891b7d58da34f698e16469f9528961af55378c3
zaknolte <127704632+zaknolte@users.noreply.github.com>
2024-06-23 16:58:15 -0700

    rename Peak to Compound

---
+++ components/Compound.py
@@ -0,0 +1,39 @@
+import numpy as np
+from scipy.signal import find_peaks
+
+from components.Calibration import Calibration
+from dataclasses import dataclass
+import json
+
+class Compound:
+    def __init__(self, name, x, height, center, width, skew) -> None:
+        self.name = name
+        self.x = x
+        self.height = height
+        self.center = center
+        self.width = width
+        self.skew = skew
+        self.area = 0
+        self.start_idx = 0
+        self.stop_idx = 0
+        self.concentration = 0
+        self.unit = "ppm"
+        self.calibration = Calibration(self)
+
+        self.y = self.create_peak()
+
+    def create_peak(self):
+        # current formula with skewness does not draw with width = 0
+        # set to minimal sharp value to improve UX and not have peaks disappearing
+        if self.width == 0:
+            self.width = 0.01
+        # https://www.desmos.com/calculator/gokr63ciym
+        return self.height * np.exp(-0.5 * ((self.x - self.center) / (self.width + (self.skew * (self.x - self.center))))**2)
+        # https://www.desmos.com/calculator/k5y9glwjee   ??
+        # https://math.stackexchange.com/questions/3605861/what-is-the-graph-function-of-a-skewed-normal-distribution-curve
+        # https://cremerlab.github.io/hplc-py/methodology/fitting.html
+    
+    def clear_integration(self):
+        self.area = 0
+        self.start_idx = 0
+        self.stop_idx = 0
